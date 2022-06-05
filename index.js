a7ad21493adaf8987f906a7a6ae5fc02b70829c9
anirudh loona <anirudhloona26@gmail.com>
2022-06-05 12:34:55 +0530

    scss is running and ejs layout is added

--- index.js
+++ index.js
@@ -3,15 +3,33 @@ const app = express();
 const db = require('./config/mongoose')
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
+const expressLayouts = require('express-ejs-layouts');
 const passport = require('passport');
 const MongoStore = require('connect-mongo');
+const sassMiddleware = require('node-sass-middleware');
 const passportLocal = require('./config/passport-local-strategy');
 const PORT = 3001;
 //from this index.js i will go to routes/index.js and from there to further routes.
 
+//add sass middleware before the server starts.
+app.use(sassMiddleware({
+    src : './assets/scss',
+    dest : './assets/css',
+    debug : true,
+    outputStyle : 'expanded',
+    prefix : '/css'
+}))
+
 app.use(express.urlencoded());
 app.use(cookieParser());
 //use express Router
+
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 app.set('view engine','ejs');
 app.set('views','./views');
