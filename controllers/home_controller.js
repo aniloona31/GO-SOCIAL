e033ee96909797baa42048730c49eb5a3ed901b4
anirudh loona <anirudhloona26@gmail.com>
2022-06-07 13:29:20 +0530

    added async await to make code look clean

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,6 @@
 const Post = require('../model/Post');
 const User = require('../model/User');
-module.exports.home = (req, res) => {
+module.exports.home = async (req, res) => {
     // isme na sirf mujhe id likhi hui mil rhi h...postgres mei kya ho rha tha ki vo apne aap populate hojate the 
     // mtlb meine reference create ki 2 tables mei table mei id dikh rhi h sirf prr jb mei query krta tha tbb sirf id
     // ni hoti vaha pura row hota h refernced table ka...isle mei seedha findByForeginKeyId() bhi ni krta
@@ -19,26 +19,42 @@ module.exports.home = (req, res) => {
     // populatng user in the posts document
     // populate mei key ka naam dalaege or uske schema mei ref hona jaroori h
     // populating multiple models means ek saath multiple models pupulate krna like nested pupulation(comment populate kiya comment ke andr ka user populate kiya)
-    Post.find({})
-    .populate('user')
-    .populate({
-        path : 'comments',
-        populate : {
-            path : 'user'
-        }
-    })
-    .exec((err, posts) => {
-        if (err) {
-            console.log('error occured while fetching posts');
-            return;
-        }
-        User.find({},(err,users) => {
+    // Post.find({})
+    // .populate('user')
+    // .populate({
+    //     path : 'comments',
+    //     populate : {
+    //         path : 'user'
+    //     }
+    // })
+    // .exec((err, posts) => {
+    //     if (err) {
+    //         console.log('error occured while fetching posts');
+    //         return;
+    //     }
+    //     User.find({},(err,users) => {
 
-            return res.render('home', {
-                title: "home",
-                posts: posts,
-                all_users : users
-            })
+    //         return res.render('home', {
+    //             title: "home",
+    //             posts: posts,
+    //             all_users : users
+    //         })
+    //     })
+    // })
+
+    //using async await for a cleaner code.posts store the any responce being returned from the querry.
+    try {
+        const posts = await Post.find({}).populate('user').populate({ path: 'comments', populate: { path: 'user' } });
+        const users = await User.find({});
+
+        console.log(users);
+        console.log(posts);
+        return res.render('home', {
+            title: "home",
+            posts: posts,
+            all_users: users
         })
-    })
+    } catch (err) {
+        console.log('error occured');
+    }
 }
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,16 +1,16 @@
 const Post = require('../model/Post');
 const Comment = require('../model/Comment');
-module.exports.posts = (req,res) =>{
+module.exports.posts = (req, res) => {
     return res.end('here are the posts');
 }
 
-module.exports.createPost = (req,res) => {
-    if(req.isAuthenticated()){
+module.exports.createPost = (req, res) => {
+    if (req.isAuthenticated()) {
         Post.create({
-            content : req.body.content,
-            user : req.user._id
-        }),(err,data)=>{
-            if(err){
+            content: req.body.content,
+            user: req.user._id
+        }), (err, data) => {
+            if (err) {
                 console.log('error occured while creating post');
                 return;
             }
@@ -19,38 +19,49 @@ module.exports.createPost = (req,res) => {
     return res.redirect('back');
 }
 
-module.exports.deletePost = (req,res) => {
+module.exports.deletePost = async (req, res) => {
     const postId = req.params.id;
-    console.log("yes");
-    Post.findById(postId,(err,post) => {
-        if(err){
-            console.log('post not found')
-            return;
-        }
-        //if i write req.user.id it is of type string and req.user._id is of type ObjectId
-        if(post.user.equals(req.user._id)){
-            // console.log(post.user , req.user._id);
-            // for(let commentId of post.comments){
-            //     Comment.findByIdAndDelete(commentId,(err,data)=>{
-            //         if(err){
-            //             console.log('error in deleting the comment');
-            //             return;
-            //         }
-            //         // console.log(data);
-            //     });
-            // }this is one way to do things
+    // console.log("yes");
+    // Post.findById(postId,(err,post) => {
+    //     if(err){
+    //         console.log('post not found')
+    //         return;
+    //     }
+    //     //if i write req.user.id it is of type string and req.user._id is of type ObjectId
+    //     if(post.user.equals(req.user._id)){
+    //         // console.log(post.user , req.user._id);
+    //         // for(let commentId of post.comments){
+    //         //     Comment.findByIdAndDelete(commentId,(err,data)=>{
+    //         //         if(err){
+    //         //             console.log('error in deleting the comment');
+    //         //             return;
+    //         //         }
+    //         //         // console.log(data);
+    //         //     });
+    //         // }this is one way to do things
+    //         post.remove();
+
+    //         //deleteMany is used to delete all thich contains this parameter
+    //         Comment.deleteMany({post : postId},(err,data) => {
+    //             if(err){
+    //                 console.log("error while deleting the comments");
+    //                 return;
+    //             }
+    //         })
+    //     }
+    // });
+    try {
+        let post = await Post.findById(postId);
+
+        if (post.user.equals(req.user._id)) {
+            let comment = await Comment.deleteMany({ post: postId });
             post.remove();
-
-            //deleteMany is used to delete all thich contains this parameter
-            Comment.deleteMany({post : postId},(err,data) => {
-                if(err){
-                    console.log("error while deleting the comments");
-                    return;
-                }
-            })
         }
-    });
 
-    res.redirect('/');
+        res.redirect('/');
+    } catch (err) {
+        console.log('error occred');
+        return;
+    }
 }
 
